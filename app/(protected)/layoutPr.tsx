20c2fcc94c82400e3b9920ecbcc8e0ceb800d8e6
Nalindadsn <nalindadsn@gmail.com>
2025-01-12 02:06:56 +0530

    a

--- app/(protected)/layoutPr.tsx
+++ app/(protected)/layoutPr.tsx
@@ -1,10 +1,3 @@
-import { Navbar } from "./_components/navbar";
-import Sidebar from '@/components/sidebar'
-export default function LayoutPr(props:any) {
-    return (
-        <>
-         
-   
-        </>
-    )
+export default function LayoutPr(props: any) {
+  return <></>;
 }
