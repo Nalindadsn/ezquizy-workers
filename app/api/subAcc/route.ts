ddfaf0e1a226e5ed96eec6f1d52dbdd8a1cf222e
Nalindadsn <nalindadsn@gmail.com>
2025-01-12 02:03:04 +0530

    a

--- app/api/subAcc/route.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const org:any =  await db.organization.findMany()
-  return new NextResponse(JSON.stringify(org), { status: 200 });
-}
