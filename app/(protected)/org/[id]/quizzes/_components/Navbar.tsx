ed65eb6ab41717418c40234e361382c5d9a7ffb5
Nalindadsn <nalindadsn@gmail.com>
2025-01-12 02:13:25 +0530

    a

--- app/(protected)/org/[id]/quizzes/_components/Navbar.tsx
+++ app/(protected)/org/[id]/quizzes/_components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { MdQuiz } from "react-icons/md";
+// import { MdQuiz } from "react-icons/md";
 import UserMenu from "./UserMenu";
 
 const Navbar = () => {
@@ -8,14 +8,9 @@ const Navbar = () => {
     <div className="pt-5 w-full">
       <div className="max-w-[1500px] mx-auto w-[90%] flex justify-between items-center border-b pb-5">
         <div>
-          <Link
-            href={"/"}
-            className="flex gap-1 items-center text-2xl"
-          >
-            <h1 className="text-dark font-bold">
-              CodeQuiz
-            </h1>
-            <MdQuiz className="text-primary" />
+          <Link href={"/"} className="flex gap-1 items-center text-2xl">
+            <h1 className="text-dark font-bold">CodeQuiz</h1>
+            {/* <MdQuiz className="text-primary" /> */}
           </Link>
         </div>
 
