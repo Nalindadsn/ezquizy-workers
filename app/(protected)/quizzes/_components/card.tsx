4a6fed501fcddae5d78d9c55d0a75625deac6b45
Nalindadsn <nalindadsn@gmail.com>
2025-01-17 09:08:54 +0530

    add

--- app/(protected)/quizzes/_components/card.tsx
+++ app/(protected)/quizzes/_components/card.tsx
@@ -1,16 +1,7 @@
-import Image from "next/image"
 import Link from "next/link";
 
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { FaEllipsisV } from "react-icons/fa";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   Dialog,
   DialogContent,
@@ -19,14 +10,14 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 import { Button } from "@/components/ui/button";
 interface IPokemon {
-  data: any
+  data: any;
 }
 import {
   Select,
@@ -36,70 +27,58 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import AddTodo from "./AddTodo";
+} from "@/components/ui/select";
 import { Badge } from "@/components/ui/badge";
 import { FileWarning, ListStart, Plane, Play, Send, Star } from "lucide-react";
 export default function PokemonCard({ data }: IPokemon) {
-
   return (
     <>
       <Card key={data?.id}>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-
-          </CardTitle>
-          <div className='text-right'>
-
-            {data?.quizzesSection.length} Sections<br />
-
-
-
+          <CardTitle className="text-sm font-medium"></CardTitle>
+          <div className="text-right">
+            {data?.quizzesSection.length} Sections
+            <br />
           </div>
         </CardHeader>
         <CardContent>
-          <Link
-            key={data?.id}
-            href={`/quizzes/${data?.id}`}
-            className="w-full"
-
-          >
+          <Link key={data?.id} href={`/quizzes/${data?.id}`} className="w-full">
             <h3 className="text-2xl font-bold">{data?.name}</h3>
-            
+
             {/* <span className="text-xs text-gray-500">id: {data?.id}</span> */}
-            </Link>
+          </Link>
 
-{data?.quizzesSection.length<1&& <div className="flex justify-center items-center  text-center mt-5"><FileWarning/> No data</div>}
-{data?.quizzesSection.map((x:any)=>{
-  return <div key={x?.id} className="flex gap-2 border  mb-1 p-2 items-center">
-              <div className="w-full ">{x?.name}
-              <p className=" text-xs text-muted-foreground">
-                {x?.questions.length} Questions
-              </p>
-                </div>
-              
-              <div className="">
-
-              <Button asChild className="h-6 p-0 px-2">
-
-                <Link href={`/quizzes/${data?.id}/section/${x?.id}`} >
-start
-                  <Play className="w-3 h-3 ml-2" />
-                </Link>
-              </Button>
-
-              </div>
+          {data?.quizzesSection.length < 1 && (
+            <div className="flex justify-center items-center  text-center mt-5">
+              <FileWarning /> No data
             </div>
-})}
-            
+          )}
+          {data?.quizzesSection.map((x: any) => {
+            return (
+              <div
+                key={x?.id}
+                className="flex gap-2 border  mb-1 p-2 items-center"
+              >
+                <div className="w-full ">
+                  {x?.name}
+                  <p className=" text-xs text-muted-foreground">
+                    {x?.questions.length} Questions
+                  </p>
+                </div>
 
-
-
-
+                <div className="">
+                  <Button asChild className="h-6 p-0 px-2">
+                    <Link href={`/quizzes/${data?.id}/section/${x?.id}`}>
+                      start
+                      <Play className="w-3 h-3 ml-2" />
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
         </CardContent>
       </Card>
-
-
     </>
-  )
+  );
 }
